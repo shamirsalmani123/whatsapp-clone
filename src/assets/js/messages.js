43a1a24e6481a40fabeccb7888b4f227d958929b
shamirsalmani123 <shamir.salmani786@gmail.com>
2022-12-14 16:34:58 +0530

    userinterface created

---
+++ src/assets/js/messages.js
@@ -0,0 +1,61 @@
+const messagesIndexes = [
+    {
+        "user": "Mukul Jangra",
+        "image": "https://randomuser.me/api/portraits/lego/8.jpg",
+        "messages": [
+            {
+                "time": "12:15",
+                "content": "Hi there Darius, are you free?",
+                "send": false
+            },
+            {
+                "time": "12:17",
+                "content": "Hey Mukul, what is it?",
+                "send": true
+            },
+            {
+                "time": "12:18",
+                "content": "We are going to play basketball tomorrow, would you like to coin?",
+                "send": false
+            },
+            {
+                "time": "12:25",
+                "content": "That seems great, add me to team!",
+                "send": true
+            },
+        ]
+    },
+    {
+        "user": "Sameer Salmani",
+        "image": "https://randomuser.me/api/portraits/lego/9.jpg",
+        "messages": [
+            {
+                "time": "23:48",
+                "content": "Hey Fellow are you coming concert tomorrow night?",
+                "send": false
+            },
+            {
+                "time": "01:15",
+                "content": "oh, sorry Elena I am going to play basketball with Stefan.",
+                "send": true
+            },
+        ]
+    },
+    {
+        "user": "Himasnhu Chauhan",
+        "image": "https://randomuser.me/api/portraits/lego/8.jpg",
+        "messages": [
+            {
+                "time": "17:12",
+                "content": "Buddy can you send me photo of your homework?",
+                "send": false
+            },
+            {
+                "time": "18:19",
+                "content": "I am on the way right now. Let me call you back.",
+                "send": true
+            },
+        ]
+    }
+];
+export default messagesIndexes;
